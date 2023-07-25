3f549d5b75f3d516fe04d7d157d8b6547ffe851f
piyanzgalina <galinapiyanzina@gmail.com>
2023-07-25 12:48:31 +0300

    add hangman game files

---
+++ src/js/main.js
@@ -0,0 +1,10 @@
+import '../css/style.css';
+import { darkModeHandle } from './utils';
+import { startGame } from './game';
+
+
+darkModeHandle();
+const startGameButton = document.getElementById('startGame');
+startGameButton.addEventListener('click',() => {
+    startGame();
+});
---
+++ src/js/utils.js
@@ -0,0 +1,20 @@
+export const darkModeHandle = () => {
+
+    const darkModeSwitcher = document.getElementById('toggleDarkMode');
+    const htmlElement = document.documentElement;
+
+    if (localStorage.getItem('mode') === 'dark') {
+        htmlElement.classList.add('dark');
+        
+    };
+
+    darkModeSwitcher.addEventListener('input', () => {
+        htmlElement.classList.toggle('dark');
+
+        if (htmlElement.classList.contains('dark')) {
+            localStorage.setItem('mode', 'dark');
+        } else {
+            localStorage.setItem('mode', 'light');
+        }
+    });
+}
